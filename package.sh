9f05e7a6b531a33d6381f6027ff278ff050094e3
ralfonso <ralfonso@gmail.com>
2008-12-04 17:48:39 -0800

    changelog update and version bump in package script

--- package.sh
+++ package.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 SELF=$(cd $(dirname $0); pwd -P)/
-VERSION=0.1.1
+VERSION=0.1.2
 
 cd "$SELF/.."
 tar jcvf theory-${VERSION}.tar.bz2 --exclude-from=theory/tar_exclude --exclude-vcs theory
