ae347345541d19183f08220513b44bb681d80209
ralfonso <ralfonso@gmail.com>
2008-12-14 08:51:00 -0800

    add album art directory creation for users installing from git

--- install.sh
+++ install.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 
 SELF=$(cd $(dirname $0); pwd -P)/
+# if the user is installing from source make sure the album img dir exists
+if [ ! -e "$SELF/theory/public/img/art/" ];
+then
+    mkdir "$SELF/theory/public/img/art/"
+fi
+
 echo "installing theory prerequisites and Python virtual environment to $SELF/env/"
 python go-pylons.py --no-site-packages "$SELF/env/" &&
 "$SELF/env/bin/easy_install" python-mpd
